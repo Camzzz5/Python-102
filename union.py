f3f7b44d2cc443b7814fa76f7f8e73ce23d6c351
Camilo Santander <camilosantander@usantotomas.edu.co>
2022-11-14 18:46:40 -0500

    mas operaciones

--- union.py
+++ union.py
@@ -2,3 +2,21 @@ A = {1,2,3}
 B = {3,4,5}
 C = A | B
 print(C)
+
+#interseccion
+
+C=A.intersection(B)
+print(C)
+C = A&B
+print(C)
+
+#Diferencia 
+C = A-B
+print(C)
+C=A.difference(B)
+print(C)
+C=B-A
+print(C)
+C=B.difference(A)
+print(C)
+#Diferencia simetrica

48adc04d3203b87d137f0d3d52eb6143fcafe1aa
Camilo Santander <camilosantander@usantotomas.edu.co>
2022-11-16 15:32:38 -0500

    hice algunos cambios

--- functions.py
+++ functions.py
@@ -1,4 +1,25 @@
 def myfunction():
+    #print("xd")
     return "esto se devuelve?"
 
 myfunction()
+
+print(myfunction())
+
+suma = lambda a, b : a + b
+
+print(suma(4,5))
+
+def function_1(x):
+    return x+1
+
+def function_2(x,ejemplo):
+    return x+ejemplo(x)
+
+result = function_2(2,function_1)
+print(result)
+
+ejemplo1 = lambda x:x+ 1
+ejemplo2 = lambda x , ej: x + ej(x)
+result = ejemplo2(2, ejemplo1)
+print(result)

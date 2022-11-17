db3da0d8cacefd17105e850b242b3840ca78bb3e
Camilo Santander <camilosantander@usantotomas.edu.co>
2022-11-16 19:41:22 -0500

    unos cambios mas

---
+++ mapz.py
@@ -0,0 +1,19 @@
+# x = 5
+# def ejemplo():
+#     x = 3
+#     return x
+# resultado = ejemplo()
+# print(resultado)
+# print(x)
+
+# x = 5
+# def ejemplo():
+#     global x
+#     x = 3
+# print(x)
+
+
+def ejemplo():
+    global x
+    x = 3
+print(x)
